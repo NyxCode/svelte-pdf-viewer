405eea84d72e4280a4e2211a387856e0d926f955
Moritz Bischof <moritz.bischof1@gmail.com>
2022-08-06 22:58:02 +0200

    update README.md, include build artifacts to speed up netlify build

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # svelte-pdf-viewer
 
-> :warning: **Here be dragons**
+> :warning: very experimental - **here be dragons**
+
+## [demo](https://svelte-pdf-viewer.nyxcode.com/)
+
+## supported renderers
+- [x] **Pdfium** *(using WASM through [pdfium-render](https://github.com/ajrcarey/pdfium-render))*
+- [ ] **PDF.js**
---
+++ pdfium/pkg/pdfium_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_pdfiumwasmrenderer_free(a: number): void;
+export function pdfiumwasmrenderer_new(a: number): void;
+export function pdfiumwasmrenderer_load_document(a: number, b: number, c: number, d: number): void;
+export function __wbg_pdfiumwasmdocument_free(a: number): void;
+export function pdfiumwasmdocument_pages(a: number): number;
+export function pdfiumwasmdocument_page_size(a: number, b: number, c: number): void;
+export function pdfiumwasmdocument_render_page(a: number, b: number, c: number, d: number, e: number): void;
+export function __wbg_pdfiumwasmpagesize_free(a: number): void;
+export function __wbg_get_pdfiumwasmpagesize_width(a: number): number;
+export function __wbg_set_pdfiumwasmpagesize_width(a: number, b: number): void;
+export function __wbg_get_pdfiumwasmpagesize_height(a: number): number;
+export function __wbg_set_pdfiumwasmpagesize_height(a: number, b: number): void;
+export function initialize_pdfium_render(a: number, b: number, c: number): number;
+export function read_block_from_callback_wasm(a: number, b: number, c: number, d: number): number;
+export function write_block_from_callback_wasm(a: number, b: number, c: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
